147a510ff502e6a4f7f3eeeca8ed2a942f8b391f
Tomasz Kukla <79609921+tomasz-kukla@users.noreply.github.com>
2021-06-07 21:19:43 +0200

    Add change credentials functionality

--- app/src/main/java/com/example/timingapp/ProfileFragment.java
+++ app/src/main/java/com/example/timingapp/ProfileFragment.java
@@ -1,10 +1,13 @@
 package com.example.timingapp;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +15,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 
 /**
@@ -24,7 +32,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class ProfileFragment extends Fragment {
 
     EditText etUsername, etPassword, etEmail, etPhone;
-    Button button;
+    Button logoutBtn, changeBtn, deleteBtn;
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -74,11 +82,15 @@ public class ProfileFragment extends Fragment {
         etEmail = view.findViewById(R.id.profile_email);
         etPhone = view.findViewById(R.id.profile_phone);
 
+        logoutBtn = view.findViewById(R.id.logout_btn);
+        changeBtn = view.findViewById(R.id.change_btn);
+        deleteBtn =view.findViewById(R.id.delete_btn);
+
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         String email = user.getEmail();
         String uid = user.getUid();
-        //String phone = user.getPhoneNumber();
+        String phone = user.getPhoneNumber();
 
 
         Toast.makeText(getActivity(),"Email is: "+ uid,Toast.LENGTH_SHORT).show();
@@ -86,7 +98,56 @@ public class ProfileFragment extends Fragment {
         SharedPreferences settings = this.getActivity().getSharedPreferences("PREFS", 0);
         etEmail.setText(settings.getString(email, email));
         etUsername.setText(settings.getString(uid, uid)); //TODO change -> uid username
-        //etPhone.setText(settings.getString(uid, uid));
+        etPhone.setText(settings.getString(phone, phone));
+
+        changeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailIO = etEmail.getText().toString().trim();
+                String passwordIO = etPassword.getText().toString().trim();
+
+                if (emailIO.isEmpty()) {
+                    etEmail.setError("Email is required");
+                    etEmail.requestFocus();
+                    return;
+                }
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(emailIO).matches()) {
+                    etEmail.setError("Please set valid email.");
+                    etEmail.requestFocus();
+                    return;
+                }
+
+                user.updateEmail(emailIO)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getActivity(),"Changes applied",Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+            if (passwordIO.length() != 0) {
+                user.updatePassword(passwordIO)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getActivity(),"Changes applied", Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+            }
+            }
+        });
+
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.delete();
+                Intent intent = new Intent(getActivity(), Login.class);
+                startActivity(intent);
+            }
+        });
+
+
 
 
 
