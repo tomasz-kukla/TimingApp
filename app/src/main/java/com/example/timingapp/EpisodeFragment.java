33eeedbeebcffee365be4d18ba97071b05e87ea8
Tomasz Kukla <79609921+tomasz-kukla@users.noreply.github.com>
2021-06-24 00:06:17 +0200

    Add get and delete functionality for isWatched episode status

--- app/src/main/java/com/example/timingapp/EpisodeFragment.java
+++ app/src/main/java/com/example/timingapp/EpisodeFragment.java
@@ -1,8 +1,11 @@
 package com.example.timingapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -121,6 +124,90 @@ public class EpisodeFragment extends Fragment {
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
 
+        //GET - info if is Watched
+        JsonPlaceHolderApi jsonPlaceHolderApiGET= retrofit.create(JsonPlaceHolderApi.class);
+        Call<EpisodeFav> callPOST = jsonPlaceHolderApiGET.getWatched(userId, show_id, season_id, episode_id);
+        callPOST.enqueue(new Callback<EpisodeFav>() {
+            @Override
+            public void onResponse(Call<EpisodeFav> call, Response<EpisodeFav> response) {
+                if(response.isSuccessful()){
+                    watchedBtn.setBackgroundColor(Color.WHITE);
+                    watchedBtn.setTextColor(Color.BLACK);
+                    watchedBtn.setText("Mark as not seen");
+                    watchedBtn.setOnClickListener(v -> {
+                        JsonPlaceHolderApi jsonPlaceHolderApiDELETE= retrofit.create(JsonPlaceHolderApi.class);
+                        Call<Void> callDELETE = jsonPlaceHolderApiDELETE.deleteWatched(userId, show_id, season_id, episode_id);
+                        callDELETE.enqueue(new Callback<Void>() {
+
+                            @Override
+                            public void onResponse(Call<Void> call2, Response<Void> response2) {
+                                Toast.makeText(getActivity(), "Marked as not seen.",Toast.LENGTH_SHORT).show();
+
+                                EpisodeFragment episodeFragment = new EpisodeFragment();
+                                Bundle args = new Bundle();
+                                args.putString("nameShow", show_name);
+                                args.putString("idShow", show_id);
+                                args.putString("noSeason", season_no);
+                                args.putString("titleSeason", season_title);
+                                args.putString("idSeason", season_id);
+                                args.putString("idEpisode", episode_id);
+                                args.putString("nameEpisode", season_title);
+//                    Toast.makeText(getActivity(), "Name: " +seriesList.get(position).getId() ,Toast.LENGTH_SHORT).show();
+                                episodeFragment.setArguments(args);
+                                //Navigate To ShowFragment to display detailed info about show
+                                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                                fragmentTransaction.add(R.id.fragment_container, episodeFragment);
+                                fragmentTransaction.addToBackStack(null);
+                                fragmentTransaction.commit();
+                            }
+                            @Override
+                            public void onFailure(Call<Void> call2, Throwable t) {
+
+                            }
+                        });
+                    });
+                } else {
+
+                    //POST add episode as watched
+                    watchedBtn.setOnClickListener(v -> {
+                        Toast.makeText(getActivity(), season_title + " marked as watched!",Toast.LENGTH_SHORT).show();
+                        EpisodeFav episodeFav = new EpisodeFav(true);
+                        JsonPlaceHolderApi jsonPlaceHolderApiPOST = retrofit.create(JsonPlaceHolderApi.class);
+                        Call<EpisodeFav> callPOST = jsonPlaceHolderApiPOST.addWatched(userId, show_id, season_id, episode_id, episodeFav);
+                        callPOST.enqueue(new Callback<EpisodeFav>() {
+                            @Override
+                            public void onResponse(Call<EpisodeFav> call1, Response<EpisodeFav> response1) {}
+                            @Override
+                            public void onFailure(Call<EpisodeFav> call1, Throwable t) {}
+                        });
+                        EpisodeFragment episodeFragment = new EpisodeFragment();
+                        Bundle args = new Bundle();
+                        args.putString("nameShow", show_name);
+                        args.putString("idShow", show_id);
+                        args.putString("noSeason", season_no);
+                        args.putString("titleSeason", season_title);
+                        args.putString("idSeason", season_id);
+                        args.putString("idEpisode", episode_id);
+                        args.putString("nameEpisode", season_title);
+//                    Toast.makeText(getActivity(), "Name: " +seriesList.get(position).getId() ,Toast.LENGTH_SHORT).show();
+                        episodeFragment.setArguments(args);
+                        //Navigate To ShowFragment to display detailed info about show
+                        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.add(R.id.fragment_container, episodeFragment);
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                    });
+                }
+            }
+            @Override
+            public void onFailure(Call<EpisodeFav> call, Throwable t) { ; }
+        });
+
+
+
+        //GET - episode info
         JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
         Call<Episode> call = jsonPlaceHolderApi.getEpisode(show_id, season_id, episode_id);
 
@@ -131,32 +218,6 @@ public class EpisodeFragment extends Fragment {
 
                 descriptionView.setText(response.body().getDescription());
                 noEpisode.setText(Integer.toString(response.body().getNoOfEpisode()));
-                String idEpisode = response.body().getId();
-
-                //POST add episode as watched
-                watchedBtn.setOnClickListener(v -> {
-                    Toast.makeText(getActivity(), season_title + " marked as watched!",Toast.LENGTH_SHORT).show();
-                    EpisodeFav episodeFav = new EpisodeFav(true);
-                    JsonPlaceHolderApi jsonPlaceHolderApiPOST = retrofit.create(JsonPlaceHolderApi.class);
-                    Call<EpisodeFav> callPOST = jsonPlaceHolderApiPOST.addWatched(userId, show_id, season_id, episode_id, episodeFav);
-                    callPOST.enqueue(new Callback<EpisodeFav>() {
-                        @Override
-                        public void onResponse(Call<EpisodeFav> call1, Response<EpisodeFav> response1) {}
-                        @Override
-                        public void onFailure(Call<EpisodeFav> call1, Throwable t) {}
-                    });
-                });
-
-                //GET
-                JsonPlaceHolderApi jsonPlaceHolderApiGET= retrofit.create(JsonPlaceHolderApi.class);
-                Call<EpisodeFav> callPOST = jsonPlaceHolderApiGET.getWatched(userId, show_id, season_id, episode_id);
-
-
-
-
-
-
-
 
             }
             @Override
