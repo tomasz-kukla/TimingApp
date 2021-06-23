8cfceb7eb61060e02977ffdb7cad4a52d16ee776
Tomasz Kukla <79609921+tomasz-kukla@users.noreply.github.com>
2021-06-23 21:50:28 +0200

    Change users credentails needed

--- app/src/main/java/com/example/timingapp/User.java
+++ app/src/main/java/com/example/timingapp/User.java
@@ -4,15 +4,13 @@ public class User {
 
     public String username;
     public String email;
-    public String phone;
 
     public User (){
 
     }
 
-    public User(String username,String email, String phone ) {
+    public User(String username,String email) {
         this.username = username;
         this.email = email;
-        this.phone = phone;
     }
 }
